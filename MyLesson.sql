1ee051ce6831edea3a68406860c13043b0af177a
gyamficodes <gyamfijohn08@gmail.com>
2024-08-07 12:03:36 +0000

    update

--- MyLesson.sql
+++ MyLesson.sql
@@ -132,5 +132,90 @@ CREATE TABLE employeer(
 
 
 
+---name constrain
+--- this wiil faild because of the chk_emp_age constrain .
+INSERT INTO employeer(emp_id , emp_name ,  emp_age ,  emp_salary)
+            VALUES(1, "John", 25, 18 );
+
+SELECT * FROM employeer;
+
+---- Alter Keyword
+--Drop A Cretain  Colum  From DB;
+ALTER TABLE employeer DROP COLUMN emp_salary ;
+
+--   Modify a certain tabel colum data From DB Table;
+ALTER TABLE employeer MODIFY COLUMN  emp_name VARCHAR(50);
+
+--- change a certain colunm name;
+ALTER TABLE employeer  CHANGE emp_name  full_name VARCHAR(50);
+
+
+--- Relational database
+-- one-to-one relation
+--one-to-many relation 
+--many-to-many relation
+
+
+--- one-to-many relation 
+
+CREATE TABLE engineer(
+    engineer_id INT AUTO_INCREMENT PRIMARY KEY,
+    engineer_name VARCHAR(255) NOT NULL,
+    profession VARCHAR(255) NOT NULL,
+    email VARCHAR(50)
+);
+
+INSERT INTO engineer(engineer_name,profession,email) 
+VALUES("Gyamfi John", "Full Stack", "info@gmail.com"),
+       ("Alice", "Frontend Developer", "info@gmail.com"),
+       ("Emma", "Full Stack", "info@gmail.com");
+
+
+
+CREATE TABLE Task (
+    task_id INT AUTO_INCREMENT PRIMARY KEY,
+    task_name VARCHAR(255),
+    task_description TEXT,
+    engineer_id INT,
+    FOREIGN KEY (engineer_id) REFERENCES engineer(engineer_id)
+);
+
+INSERT INTO Task (task_name, task_description, engineer_id)
+VALUES 
+    ('task 1', 'Design The UI in Figma', 1),
+    ('task 2', 'Build the server in bun', 2),
+    ('task 3', 'Code the UI in Tailwind CSS', 1);
+ 
+--- cross join 
+SELECT * FROM engineer CROSS JOIN task;
+
+
+SELECT * FROM engineer;
+
+
+CREATE TABLE  Musician(
+    dev_id INT AUTO_INCREMENT PRIMARY KEY,
+    dev_name VARCHAR(255),
+    dev_info TEXT
+);
+
+INSERT INTO  musician(dev_name,dev_info)
+VALUES("Rema","He is Afrobeat Singer");
+                     
+SELECT * FROM musician WHERE dev_id <= 1;
+
+CREATE TABLE  Job(
+    job_id INT AUTO_INCREMENT PRIMARY KEY,
+    job_name VARCHAR(255),
+    job_info TEXT,
+    dev_id INT ,
+   FOREIGN KEY ( dev_id) REFERENCES Musician( dev_id)
+);
+
+INSERT INTO job(job_name,job_info,dev_id)
+VALUES("Ragea","From nigerial",1)
+
+
+SELECT * FROM job CROSS JOIN musician;
 
 
