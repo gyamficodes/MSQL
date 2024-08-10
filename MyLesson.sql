5122671245d21ae5b75da214a7f5db84ada4c701
gyamficodes <gyamfijohn08@gmail.com>
2024-08-10 08:49:16 +0000

    update

--- MyLesson.sql
+++ MyLesson.sql
@@ -240,8 +240,19 @@ CREATE TABLE Movies (
     genre VARCHAR(100)
 );
 
-INSERT INTO Movies(movie_id, title, year, director, genre)
- VALUES(),
+INSERT INTO Movies( title, year, director, genre )
+ VALUES ('The Shawshank Redemption',1994,'Frank Darabont','Drama'),
+('The Godfather',1972,'Francis Ford Coppola','Crime'),
+('The Godfather: Part II',1974,'Francis Ford Coppola','Crime'),
+('The Dark Knight',2008,'Christopher Nolan','Action'),
+('12 Angry Men',1957,'Sidney Lumet','Drama'),
+('Schindler''s List',1993,'Steven Spielberg','Drama'),
+('Pulp Fiction',1994,'Quentin Tarantino','Crime'),
+('The Lord of the Rings: The Return of the King',2003,'Peter Jackson','Action'),
+('The Good, the Bad and the Ugly',1966,'Sergio Leone','Western'),
+('Fight Club',1999,'David Fincher','Drama');
+
+
 
 
 
@@ -250,6 +261,50 @@ CREATE TABLE Person (
     name VARCHAR(100) NOT NULL,
     age INT,
     email VARCHAR(100),
-    genre VARCHAR(100),
+    gender VARCHAR(100),
     country VARCHAR(100)
+);
+
+INSERT INTO PERSON(name,age,email,gender,country)
+VALUES ('Tom Hanks', 63, 'Vx5uZ@example.com', 'Male', 'United States'),
+    ('Brad Pitt', 59, 'U7lZD@example.com', 'Male', 'United States'),
+    ('Leonardo DiCaprio', 47, 'Vx5uZ@example.com', 'Male', 'United States'),
+    ('Margot Robbie', 35, 'Vx5uZ@example.com', 'Female', 'United States'),
+    ('Kate Winslet', 32, 'Vx5uZ@example.com', 'Female', 'United States'),
+    ('Christian Bale', 45, 'Vx5uZ@example.com', 'Male', 'United States'),
+    ('Heath Ledger', 49, 'Vx5uZ@example.com', 'Male', 'United States'),
+    ('Gary Oldman', 51, 'Vx5uZ@example.com', 'Male', 'United States'),
+    ('Tom Cruise', 47, 'Vx5uZ@example.com', 'Male', 'United States');
+
+
+
+
+
+CREATE TABLE  comments(
+    comment_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    comment_content TEXT NOT NULL,
+    movie_id INT NOT NULL,
+     person_id INT NOT NULL,
+    FOREIGN KEY ( movie_id) REFERENCES Movies(movie_id),
+    FOREIGN KEY (person_id) REFERENCES Person(person_id)
 )
+
+INSERT INTO comments(movie_id, person_id, comment_content)
+VALUES (1,1,'I really like this movie.'),
+       (2, 2, 'I really like this movie.'),
+       (3, 3, 'I really like this movie.'),
+       (4, 4, 'I really like this movie.'),
+       (5, 5, 'I really like this movie.'),
+       (6, 6, 'I really like this movie.'),
+       (7, 7, 'I really like this movie.'),
+       (8, 8, 'I really like this movie.'),
+       (9, 9, 'I really like this movie.');
+
+
+SELECT * FROM movies CROSS JOIN person , comments;
+
+SELECT * FROM movies LEFT JOIN person  ON  movies.movie_id = person.person_id;
+
+SELECT * FROM movies INNER JOIN person ON movies.movie_id = person.person_id;
+
+SELECT * FROM movies  RIGHT JOIN person   on    movies.movie_id = person.person_id  ;
